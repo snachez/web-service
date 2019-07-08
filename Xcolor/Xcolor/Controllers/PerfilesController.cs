ec3aae219edf28e677c27139b09309949aa33267
jefry sanchez <jefrysanchezblandino@outlook.es>
2019-07-07 21:29:01 -0600

    Web service

---
+++ Xcolor/Xcolor/Controllers/PerfilesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Xcolor.Models;
+
+namespace Xcolor.Controllers
+{
+    public class PerfilesController : Controller
+    {
+        private xcolorEntities db = new xcolorEntities();
+
+        // GET: Perfiles
+        public ActionResult Index()
+        {
+            return View(db.Perfil.ToList());
+        }
+
+        // GET: Perfiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Perfil perfil = db.Perfil.Find(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(perfil);
+        }
+
+        // GET: Perfiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Perfiles/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,nombre,estado")] Perfil perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Perfil.Add(perfil);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(perfil);
+        }
+
+        // GET: Perfiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Perfil perfil = db.Perfil.Find(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(perfil);
+        }
+
+        // POST: Perfiles/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,nombre,estado")] Perfil perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(perfil).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(perfil);
+        }
+
+        // GET: Perfiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Perfil perfil = db.Perfil.Find(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(perfil);
+        }
+
+        // POST: Perfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Perfil perfil = db.Perfil.Find(id);
+            db.Perfil.Remove(perfil);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Xcolor/Xcolor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Xcolor.Startup))]
+namespace Xcolor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
